68109bd29af147076c45ae4c41382447caf55c25
merochd <33989674+merochd@users.noreply.github.com>
2024-10-15 11:58:43 +0200

    fin seance

---
+++ Assets/Scripts/player_movement_manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player_movement_manager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    
+    void Start()
+    {
+        playerInput = Get
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
