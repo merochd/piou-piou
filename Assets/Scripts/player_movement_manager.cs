0c475bb0fed3896a30fe4e6c4a07d396330d584a
JaroHds <schmutzjarod@gmail.com>
2024-11-04 16:47:36 +0100

    jsp frr

--- Assets/Scripts/player_movement_manager.cs
+++ Assets/Scripts/player_movement_manager.cs
@@ -7,15 +7,21 @@ public class player_movement_manager : MonoBehaviour
 {
     private PlayerInput playerInput;
     private InputAction moveAction;
-    
+
     void Start()
     {
-        playerInput = Get
+        // Assigner le composant PlayerInput
+        playerInput = GetComponent<PlayerInput>();
+
+        // S'assurer que moveAction est configuré correctement
+        moveAction = playerInput.actions["Move"];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Exemple de récupération des données de mouvement
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        Debug.Log("Déplacement : " + move);
     }
 }
